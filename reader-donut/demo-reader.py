b4a4fcc06b2612c5cec325e9b83c1c4d985722c5
Eugenio Culurciello <culurciello@gmail.com>
2023-05-08 14:36:10 -0400

    example of donut reading driving license

---
+++ reader-donut/demo-reader.py
@@ -0,0 +1,54 @@
+import re
+import argparse
+from PIL import Image
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+import torch
+
+title = 'Document to JSON / text converter'
+
+def get_args():
+    parser = argparse.ArgumentParser(description=title)
+    arg = parser.add_argument
+    arg('--i', type=str, default="../data/img/DL-test.jpg",  help='input image file')
+    args = parser.parse_args()
+    return args
+
+def doc2json(image):
+    processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+    model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
+    # prepare decoder inputs
+    task_prompt = "<s_cord-v2>"
+    decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+
+    outputs = model.generate(
+        pixel_values.to(device),
+        decoder_input_ids=decoder_input_ids.to(device),
+        max_length=model.decoder.config.max_position_embeddings,
+        early_stopping=True,
+        pad_token_id=processor.tokenizer.pad_token_id,
+        eos_token_id=processor.tokenizer.eos_token_id,
+        use_cache=True,
+        num_beams=1,
+        bad_words_ids=[[processor.tokenizer.unk_token_id]],
+        return_dict_in_generate=True,
+    )
+
+    sequence = processor.batch_decode(outputs.sequences)[0]
+    sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+    sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+    return processor.token2json(sequence)
+
+
+if __name__ == "__main__":
+    args = get_args() # all input arguments
+    print(title)
+    # load document image
+    image = Image.open(args.i)
+    json = doc2json(image)
+    print(json)
